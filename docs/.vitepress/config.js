8e0d4c1aa257aa78a1d559bc5e9c858a65afe689
wfnuser <wfnuser@hotmail.com>
2024-06-24 19:53:56 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "YouBet",
+  themeConfig: {
+    sidebar: {
+      "/": [
+        {
+          text: "Introduction",
+          link: "/",
+        },
+        {
+          text: "Quick Start",
+          items: [{ text: "Hello, World!", link: "/quickstart/hello-world" }],
+        },
+      ],
+    },
+  },
+};
